bebac5934b67f9629abd828fd2d4fa67d3bd0744
Keny <kenyda25@gmail.com>
2021-10-14 18:17:14 +0300

    Add authorization

--- src/pages/Authorization/LoginComponent.vue
+++ src/pages/Authorization/LoginComponent.vue
@@ -19,6 +19,10 @@
                    class=""
                    @click="submit()">Submit</el-button>
       </el-form-item>
+      <el-alert @close="authError = false"
+                v-if="authError"
+                title="Неверный email или пароль"
+                type="error" />
     </el-form>
   </el-card>
 </div>
@@ -27,11 +31,12 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
 import {
-  ElForm,
+  ElButton,
   ElCard,
+  ElForm,
   ElFormItem,
   ElInput,
-  ElButton,
+  ElAlert,
 } from 'element-plus';
 
 export default defineComponent({
@@ -42,7 +47,9 @@ export default defineComponent({
     ElFormItem,
     ElInput,
     ElButton,
+    ElAlert,
   },
+
   setup() {
     const formRef = ref<InstanceType<typeof ElForm>>();
 
@@ -51,6 +58,7 @@ export default defineComponent({
 
   data() {
     return {
+      authError: false,
       form: {
         email: '',
         password: '',
@@ -65,7 +73,12 @@ export default defineComponent({
           {
             type: 'email',
             message: 'Некорректное значение',
-            trigger: ['blur'],
+            trigger: 'blur',
+          },
+          {
+            max: 255,
+            message: 'Значение не должно быть длиннее 255',
+            trigger: ['change', 'blur'],
           },
         ],
         password: [
@@ -74,20 +87,59 @@ export default defineComponent({
             message: 'Поле обязательно для заполнения',
             trigger: 'blur',
           },
+          {
+            max: 128,
+            message: 'Значение не должно быть длиннее 128',
+            trigger: ['change', 'blur'],
+          },
         ],
       },
     };
   },
   methods: {
-    submit() {
+    async submit() {
       if (this.formRef === undefined) return;
+      let isValid: boolean | undefined = false;
       this.formRef.validate((valid: boolean | undefined) => {
-        if (valid) {
-          console.log('valid');
-        }
+        isValid = valid;
       });
+      if (isValid) {
+        const tokenData = await this.getToken();
+        if (tokenData.access_token) {
+          this.$store.dispatch('saveToken', tokenData.access_token);
+          localStorage.setItem('token', JSON.stringify(tokenData.access_token));
+          this.$router.push('/');
+        } else this.authError = true;
+      }
+    },
+
+    async getToken() {
+      const formData = new FormData();
+
+      formData.append('username', this.form.email);
+      formData.append('password', this.form.password);
+
+      try {
+        const response = await fetch('http://localhost:8081/api/auth/jwt/login', {
+          method: 'POST',
+          body: formData,
+        });
+        return await response.json();
+      } catch (error) {
+        console.log(error);
+        return null;
+      }
     },
   },
+
+  mounted() {
+    const localStorageToken = JSON.parse(localStorage.getItem('token') as string);
+    if (localStorageToken) {
+      this.$store.dispatch('saveToken', localStorageToken);
+      this.$router.push('/');
+    }
+  },
+
 });
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,18 @@ module.exports = {
       sass: {
         prependData: ' @import "@/assets/styles.scss"; '
       }
+    },
+  },
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true
+      },
+      '^/foo': {
+        target: 'http://localhost:8081'
+      }
     }
-  }
+  },
 }
