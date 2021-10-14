8de91d47beabbea91f373cd82b933cb2441b2920
Keny <kenyda25@gmail.com>
2021-10-14 13:08:22 +0300

    Added login form

--- src/pages/Authorization/LoginComponent.vue
+++ src/pages/Authorization/LoginComponent.vue
@@ -2,9 +2,22 @@
 <div class="authorization-card">
   <el-card>
     <h1>Login</h1>
-    <el-form ref="form" :model="form">
-      <el-form-item label="Activity name">
-        <el-input v-model="form.name"></el-input>
+    <el-form ref="formRef" :model="form" :rules="rules">
+      <el-form-item prop="email" label="Email">
+        <el-input :clearable="true" v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item label="Password" prop="password">
+        <el-input
+          v-model="form.password"
+          type="password"
+          autocomplete="off"
+          :clearable="true"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary"
+                   class=""
+                   @click="submit()">Submit</el-button>
       </el-form-item>
     </el-form>
   </el-card>
@@ -12,12 +25,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import {
   ElForm,
   ElCard,
   ElFormItem,
   ElInput,
+  ElButton,
 } from 'element-plus';
 
 export default defineComponent({
@@ -27,13 +41,52 @@ export default defineComponent({
     ElCard,
     ElFormItem,
     ElInput,
+    ElButton,
   },
+  setup() {
+    const formRef = ref<InstanceType<typeof ElForm>>();
+
+    return { formRef };
+  },
+
   data() {
     return {
       form: {
-        name: '',
+        email: '',
+        password: '',
+      },
+      rules: {
+        email: [
+          {
+            required: true,
+            message: 'Поле обязательно для заполнения',
+            trigger: 'blur',
+          },
+          {
+            type: 'email',
+            message: 'Некорректное значение',
+            trigger: ['blur'],
+          },
+        ],
+        password: [
+          {
+            required: true,
+            message: 'Поле обязательно для заполнения',
+            trigger: 'blur',
+          },
+        ],
       },
     };
+  },
+  methods: {
+    submit() {
+      if (this.formRef === undefined) return;
+      this.formRef.validate((valid: boolean | undefined) => {
+        if (valid) {
+          console.log('valid');
+        }
+      });
+    },
   },
 });
 </script>
